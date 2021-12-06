5566df487bacf4c1eaeaae44d1803dbbc7ec180c
Chris Rabl <rabl.christopher@gmail.com>
2021-12-05 23:05:26 -0700

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rs-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class="container-centered">
+      <div>
+        <div class="logo">
+          <h1>Redefine Software</h1>
+        </div>
+        <div class="tagline">
+          <p>what's an app?</p>
+        </div>
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [`
+    .container-centered {
+      display: flex; 
+      align-items: center; 
+      justify-content: center;
+      width: 100vw;
+      height: 100vh;
+    }
+
+    .tagline {
+      text-align: center;
+      margin-top: 12px;
+      color: #999;
+    }
+  `]
 })
 export class AppComponent {
   title = 'redefine-website';
